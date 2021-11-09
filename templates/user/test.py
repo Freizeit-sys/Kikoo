85173a28b3335546566488ce18e7ee2b4dc24317
Freizeit <work16087y0rha@gmail.com>
2021-11-09 11:14:58 +0900

    add home_page

---
+++ templates/user/test.py
@@ -0,0 +1,16 @@
+# print('Hello World!')
+
+def function():
+    """This is a function
+
+    Returns:
+        str: Is test huh?
+    """
+    return 'yes'
+
+isTest = False
+
+if isTest:
+    print(function())
+else:
+    print('no')
